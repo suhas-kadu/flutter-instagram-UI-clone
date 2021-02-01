ae8cc22f6ba77a505c062b3e129fab8e1e4fd090
suhas-kadu <kadusuhas72@gmail.com>
2021-02-01 20:11:24 +0530

    new post screen completed

--- lib/views/post_screen.dart
+++ lib/views/post_screen.dart
@@ -3,8 +3,146 @@ import 'package:flutter/material.dart';
 class PostScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
       
+      appBar: AppBar(
+        
+        backgroundColor: Colors.white,
+        leading: Icon(
+          Icons.close,
+          color: Colors.black,
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            DropdownButton(
+                elevation: 0,
+                icon: Icon(Icons.arrow_drop_down, color: Colors.black),
+                hint: Text("Gallery",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold)),
+                items: [DropdownMenuItem(child: Text(""))],
+                onChanged: null),
+            Icon(Icons.arrow_forward, color: Colors.blue)
+          ],
+        ),
+        elevation: 0.0,
+      ),
+      body: ListView(
+        children: [
+          Container(
+            color: Colors.black,
+            height: MediaQuery.of(context).size.height / 2,
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Image(
+                  height: MediaQuery.of(context).size.height / 2,
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://images.unsplash.com/photo-1611858368635-e79fc2a7fd77?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.black38),
+                        child: Icon(
+                          Icons.crop,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                            color: Colors.black38,
+                            borderRadius: BorderRadius.circular(25)),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.photo_library,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              "SELECT MULTIPLE",
+                              style: TextStyle(color: Colors.white),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          GridView.count(
+            shrinkWrap: true,
+            physics: ClampingScrollPhysics(),
+            crossAxisCount: 4,
+            crossAxisSpacing: 1,
+            mainAxisSpacing: 1,
+            childAspectRatio: 1,
+            children: [
+              Image(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://images.unsplash.com/photo-1611367540722-9825e219be45?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI0fHRvd0paRnNrcEdnfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")),
+              Image(
+                  height: MediaQuery.of(context).size.height / 6,
+                  width: MediaQuery.of(context).size.width / 4,
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://images.unsplash.com/photo-1611367540722-9825e219be45?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI0fHRvd0paRnNrcEdnfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")),
+              Image(
+                  height: MediaQuery.of(context).size.height / 6,
+                  width: MediaQuery.of(context).size.width / 4,
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://images.unsplash.com/photo-1611367540722-9825e219be45?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI0fHRvd0paRnNrcEdnfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")),
+              Image(
+                  height: MediaQuery.of(context).size.height / 6,
+                  width: MediaQuery.of(context).size.width / 4,
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://images.unsplash.com/photo-1611367540722-9825e219be45?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI0fHRvd0paRnNrcEdnfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")),
+              Image(
+                  height: MediaQuery.of(context).size.height / 6,
+                  width: MediaQuery.of(context).size.width / 4,
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://images.unsplash.com/photo-1611367540722-9825e219be45?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI0fHRvd0paRnNrcEdnfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")),
+              Image(
+                  height: MediaQuery.of(context).size.height / 6,
+                  width: MediaQuery.of(context).size.width / 4,
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://images.unsplash.com/photo-1611367540722-9825e219be45?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI0fHRvd0paRnNrcEdnfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")),
+              Image(
+                  height: MediaQuery.of(context).size.height / 6,
+                  width: MediaQuery.of(context).size.width / 4,
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://images.unsplash.com/photo-1611367540722-9825e219be45?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI0fHRvd0paRnNrcEdnfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")),
+              Image(
+                  height: MediaQuery.of(context).size.height / 6,
+                  width: MediaQuery.of(context).size.width / 4,
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://images.unsplash.com/photo-1611367540722-9825e219be45?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI0fHRvd0paRnNrcEdnfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"))
+            ],
+          )
+        ],
+      ),
     );
   }
 }
